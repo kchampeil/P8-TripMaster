fe2b3a0a72f3ca0bececd6a26b605f918b771648
Karine CHAMPEIL <kchampeilpro@gmail.com>
2021-08-30 11:43:09 +0200

    initializeInternalUsers externalized from TourGuideService to InternalTestingInitializationService

---
+++ TourGuide/src/main/java/tourGuide/service/InternalTestingInitializationService.java
@@ -0,0 +1,87 @@
+package tourGuide.service;
+
+import gpsUtil.location.Location;
+import gpsUtil.location.VisitedLocation;
+import lombok.extern.slf4j.Slf4j;
+import org.javamoney.moneta.Money;
+import tourGuide.helper.InternalTestHelper;
+import tourGuide.user.User;
+import tourGuide.user.UserPreferences;
+
+import javax.money.CurrencyUnit;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class InternalTestingInitializationService {
+
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Initialization of users for internal testing
+     *
+     * @param internalUserMap
+     */
+    public static Map<String, User> initializeInternalUsers(Map<String, User> internalUserMap) {
+        IntStream.range(0, InternalTestHelper.getInternalUserNumber()).forEach(i -> {
+            String userName = "internalUser" + i;
+            String phone = "000";
+            String email = userName + "@tourGuide.com";
+            User user = new User(UUID.randomUUID(), userName, phone, email);
+            generateUserLocationHistory(user);
+            generateUserPreferences(user);
+
+            internalUserMap.put(userName, user);
+        });
+        log.debug("Created " + InternalTestHelper.getInternalUserNumber() + " internal test users.");
+        return internalUserMap;
+    }
+
+    private static void generateUserLocationHistory(User user) {
+        IntStream.range(0, 3).forEach(i -> {
+            user.addToVisitedLocations(new VisitedLocation(user.getUserId(),
+                    new Location(generateRandomLatitude(), generateRandomLongitude()), getRandomTime()));
+        });
+    }
+
+    private static double generateRandomLongitude() {
+        double leftLimit = -180;
+        double rightLimit = 180;
+        return leftLimit + RANDOM.nextDouble() * (rightLimit - leftLimit);
+    }
+
+    private static double generateRandomLatitude() {
+        double leftLimit = -85.05112878;
+        double rightLimit = 85.05112878;
+        return leftLimit + RANDOM.nextDouble() * (rightLimit - leftLimit);
+    }
+
+    private static Date getRandomTime() {
+        LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
+        return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
+    }
+
+    private static void generateUserPreferences(User user) {
+        UserPreferences userPreferences = new UserPreferences();
+
+        userPreferences.setAttractionProximity(100 + RANDOM.nextInt(Integer.MAX_VALUE - 100));
+        userPreferences.setLowerPricePoint(getRandomPrice(10, userPreferences.getCurrency()));
+        userPreferences.setHighPricePoint(
+                userPreferences.getLowerPricePoint().add(getRandomPrice(10000, userPreferences.getCurrency())));
+        userPreferences.setTripDuration(RANDOM.nextInt(45));
+        userPreferences.setNumberOfAdults(1 + RANDOM.nextInt(3));
+        userPreferences.setNumberOfChildren(RANDOM.nextInt(8));
+        userPreferences.setTicketQuantity(userPreferences.getNumberOfAdults() + userPreferences.getNumberOfChildren());
+
+        user.setUserPreferences(userPreferences);
+    }
+
+    private static Money getRandomPrice(int max, CurrencyUnit currencyUnit) {
+        return Money.of(RANDOM.nextInt(max), currencyUnit);
+    }
+}
--- TourGuide/src/main/java/tourGuide/service/TourGuideService.java
+++ TourGuide/src/main/java/tourGuide/service/TourGuideService.java
@@ -4,14 +4,12 @@ import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
-import org.javamoney.moneta.Money;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import tourGuide.dto.CurrentLocationDto;
 import tourGuide.dto.NearByAttractionDto;
 import tourGuide.dto.UserPreferencesDto;
-import tourGuide.helper.InternalTestHelper;
 import tourGuide.tracker.Tracker;
 import tourGuide.user.User;
 import tourGuide.user.UserPreferences;
@@ -19,19 +17,12 @@ import tourGuide.user.UserReward;
 import tripPricer.Provider;
 import tripPricer.TripPricer;
 
-import javax.money.CurrencyUnit;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static tourGuide.constants.TourGuideExceptionConstants.USER_DOES_NOT_EXIST;
 
@@ -45,7 +36,13 @@ public class TourGuideService {
     public final Tracker tracker;
     boolean testMode = true;
 
-    public TourGuideService(GpsUtil gpsUtil, RewardsService rewardsService, TripPricer tripPricer, IUserPreferencesService userPreferencesService) {
+    private static final String TRIP_PRICER_API_KEY = "test-server-api-key";
+    // Database connection will be used for external users,
+    // but for testing purposes internal users are provided and stored in memory
+    private final Map<String, User> internalUserMap = new HashMap<>();
+
+    public TourGuideService(GpsUtil gpsUtil, RewardsService rewardsService,
+                            TripPricer tripPricer, IUserPreferencesService userPreferencesService) {
         this.gpsUtil = gpsUtil;
         this.rewardsService = rewardsService;
         this.tripPricer = tripPricer;
@@ -54,7 +51,7 @@ public class TourGuideService {
         if (testMode) {
             logger.info("TestMode enabled");
             logger.debug("Initializing users");
-            initializeInternalUsers();
+            InternalTestingInitializationService.initializeInternalUsers(internalUserMap);
             logger.debug("Finished initializing users");
         }
         tracker = new Tracker(this);
@@ -188,72 +185,4 @@ public class TourGuideService {
             }
         });
     }
-
-    /**********************************************************************************
-     *
-     * Methods Below: For Internal Testing
-     *
-     **********************************************************************************/
-    private static final String TRIP_PRICER_API_KEY = "test-server-api-key";
-    private static final Random RANDOM = new Random();
-    // Database connection will be used for external users, but for testing purposes internal users are provided and stored in memory
-    private final Map<String, User> internalUserMap = new HashMap<>();
-
-    private void initializeInternalUsers() {
-        IntStream.range(0, InternalTestHelper.getInternalUserNumber()).forEach(i -> {
-            String userName = "internalUser" + i;
-            String phone = "000";
-            String email = userName + "@tourGuide.com";
-            User user = new User(UUID.randomUUID(), userName, phone, email);
-            generateUserLocationHistory(user);
-            generateUserPreferences(user);
-
-            internalUserMap.put(userName, user);
-        });
-        logger.debug("Created " + InternalTestHelper.getInternalUserNumber() + " internal test users.");
-    }
-
-    private void generateUserLocationHistory(User user) {
-        IntStream.range(0, 3).forEach(i -> {
-            user.addToVisitedLocations(new VisitedLocation(user.getUserId(),
-                    new Location(generateRandomLatitude(), generateRandomLongitude()), getRandomTime()));
-        });
-    }
-
-    private double generateRandomLongitude() {
-        double leftLimit = -180;
-        double rightLimit = 180;
-        return leftLimit + RANDOM.nextDouble() * (rightLimit - leftLimit);
-    }
-
-    private double generateRandomLatitude() {
-        double leftLimit = -85.05112878;
-        double rightLimit = 85.05112878;
-        return leftLimit + RANDOM.nextDouble() * (rightLimit - leftLimit);
-    }
-
-    private Date getRandomTime() {
-        LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
-        return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
-    }
-
-    private void generateUserPreferences(User user) {
-        UserPreferences userPreferences = new UserPreferences();
-
-        userPreferences.setAttractionProximity(100 + RANDOM.nextInt(Integer.MAX_VALUE - 100));
-        userPreferences.setLowerPricePoint(getRandomPrice(10, userPreferences.getCurrency()));
-        userPreferences.setHighPricePoint(
-                userPreferences.getLowerPricePoint().add(getRandomPrice(10000, userPreferences.getCurrency())));
-        userPreferences.setTripDuration(RANDOM.nextInt(45));
-        userPreferences.setNumberOfAdults(1 + RANDOM.nextInt(3));
-        userPreferences.setNumberOfChildren(RANDOM.nextInt(8));
-        userPreferences.setTicketQuantity(userPreferences.getNumberOfAdults() + userPreferences.getNumberOfChildren());
-
-        user.setUserPreferences(userPreferences);
-    }
-
-    private Money getRandomPrice(int max, CurrencyUnit currencyUnit) {
-        return Money.of(RANDOM.nextInt(max), currencyUnit);
-    }
-
 }
